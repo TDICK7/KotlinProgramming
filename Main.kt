cac92564940ec90c3f7fcac5a38bca1e37418c4d
Trevor <trevordick07@gmail.com>
2023-12-18 18:11:36 -0500

    commit basic code

--- Main.kt
+++ Main.kt
@@ -1,1 +1,29 @@
-//Commit
+const val HERO_NAME = "Lorna"
+
+fun main(){
+
+    println("The Hero announces the presence to the world. People are fearful")
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    if(playerLevel == 1) {
+        println("")
+    } else if(playerLevel <= 5){
+        println("")
+    }else if(playerLevel == 6){
+        println("")
+    }else if(playerLevel == 7){
+        println("")
+    }else if(playerLevel == 8){
+        println("")
+    }else if(playerLevel == 9){
+        println("")
+    }
+
+    println("Time Passes...")
+    println("The Hero returns from her quest")
+
+    playerLevel += 1
+    println(playerLevel)
+}
