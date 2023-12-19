45baabc74d27ef537936162b91feaac1f5463e58
Trevor <trevordick07@gmail.com>
2023-12-18 21:20:34 -0500

    Addded gitIgnore and main.kt

--- Main.kt
+++ Main.kt
@@ -2,24 +2,34 @@ const val HERO_NAME = "Lorna"
 
 fun main(){
 
+    var playerLevel = 5
     println("The Hero announces the presence to the world. People are fearful")
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
 
-    if(playerLevel == 1) {
-        println("")
-    } else if(playerLevel <= 5){
-        println("")
-    }else if(playerLevel == 6){
-        println("")
-    }else if(playerLevel == 7){
-        println("")
-    }else if(playerLevel == 8){
-        println("")
-    }else if(playerLevel == 9){
-        println("")
+    val quest: String = if(playerLevel == 1) {
+        "Test"
+    } else if (playerLevel <= 5) {
+        val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+       if(canTalkToBarbarians) {
+          "Convince the Barbarians to call of invasion"
+        } else {
+           "Save town from barbarians"
+       }
+    } else if(playerLevel == 6) {
+        "Test"
+    } else if(playerLevel == 7) {
+        "Test"
+    } else if(playerLevel == 8) {
+        "Test"
+    } else {
+        "Test"
     }
+    println("The hero approaches the bounty boar: It Reads...")
+    println(quest)
 
     println("Time Passes...")
     println("The Hero returns from her quest")
